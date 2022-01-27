8e03ecddc9665fcc235e4947fc994e873a437d4f
Sodaman <mattrick2214@gmail.com>
2022-01-27 01:19:26 -0600

    updated votesRoutes.js and tested with Insomnia

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    users: 'root',
+    user: 'root',
     password: 'Rager666!',
     database: 'election'
 });
